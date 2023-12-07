c72dc9464308a66b053bd8bc993affc4ca720452
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2023-12-07 22:45:40 +0530

    datatypes

---
+++ Basics/dataTypes/dataTypes.js
@@ -0,0 +1,32 @@
+
+
+// strigs 
+
+var name = "sahil";
+
+
+// number 
+
+var age = 23;
+
+
+// Boolean 
+
+var isLogeedIn = ture; 
+
+// null => standalone value
+
+let myVariable = null;
+
+console.log(myVariable);
+
+// null is used when you have to give empty value to any variable and null is empty value 
+
+
+// undefined 
+
+var a;
+
+console.log(a);
+
+// console.log(a) returns undefined value cause we created variable but not defined any value to it 

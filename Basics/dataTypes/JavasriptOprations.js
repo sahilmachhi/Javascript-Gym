841f64129513ab7b3feb0382e9f2485f281be123
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2023-12-09 23:15:53 +0530

    commit

---
+++ Basics/dataTypes/JavasriptOprations.js
@@ -0,0 +1,49 @@
+// operators in javascript 
+
+// Arithmetic operators 
+
+
+let a = 3;
+let b = 4;
+
+// addition +
+console.log("a + b = ", a + b);
+
+// substraction - 
+console.log("a - b = ", a - b);
+
+// multiplication 
+console.log("a * b = ", a * b);
+
+// Exponentiation ** 
+console.log("a ** b = ", a ** b);
+
+// division /
+console.log("a / b = ", a / b);
+
+// modulus % 
+console.log("a % b = ", a % b);
+
+// The modulus (`%`) operator in JavaScript returns the remainder of a division operation. It's often used to check if a number is even or odd, or to cycle through a set of values.
+console.log(10 % 3);  // 1 (10 divided by 3 is 3 with a remainder of 1)
+
+
+// diffrence between A++ or A-- and ++A or --A 
+
+var A = 10;
+
+console.log(++A);
+console.log(A);
+
+// ++A is first increment the number then print it same as --A
+
+var B = 10;
+
+console.log(B++);
+console.log(B);
+
+// where A++ is first print the number then increment it so you can see its effect after another console log same goes with A--
+
+
+// assignment oprators 
+

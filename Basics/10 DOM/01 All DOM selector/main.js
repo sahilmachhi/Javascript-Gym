652a7644d8a14339e0d4c5c2524c5239b636cb1f
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2023-12-16 16:27:52 +0530

    commit

---
+++ Basics/10 DOM/01 All DOM selector/main.js
@@ -0,0 +1,16 @@
+// get element by its id 
+
+// get element by its id name 
+document.getElementById('heading');
+
+// get element's inner html 
+document.getElementById('heading').id;
+
+//get element's className by using getElementById
+document.getElementById('heading').className;
+
+// getAttribute is a method in JavaScript that is used to retrieve the value of a specified attribute of an HTML element. When you're working with HTML elements in the Document Object Model (DOM), each element can have various attributes like id, class, src, href, etc.
+document.getElementById('heading').getAttribute('id');
+document.getElementById('heading').getAttribute('class');
+
+

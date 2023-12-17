cfed28755a348d08f12441f9383a86b26ca7228a
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2023-12-17 14:44:06 +0530

    commit

--- Basics/10 DOM/01 All DOM selector/main.js
+++ Basics/10 DOM/01 All DOM selector/main.js
@@ -1,4 +1,4 @@
-// get element by its id 
+// get element by its id +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 // get element by its id name 
 document.getElementById('heading');
@@ -14,3 +14,61 @@ document.getElementById('heading').getAttribute('id');
 document.getElementById('heading').getAttribute('class');
 
 
+
+// same goes with get element by its classname +++++++++++++++++++++++++++++++++++++++++
+
+document.getElementsByClassName('heading')
+// note: when we have multiple element with same classname it and we use getElementByClassName it returns HTMLCollection list 
+
+
+// diffrence between innerText, textContent and innerHTML+++++++++++++++++++++++++++++++
+
+const title = document.getElementById("title");
+
+title.innerHTML 
+// it returns whole html inside of element as it is like 
+// 'this is Document Object Model<span>test text hello world!</span>' output when console.log();
+
+title.innerText
+// this return text inside the html element but only visible text it will not text which is hidden by css
+// 'this is Document Object Model' output when console.log();
+
+title.textContent
+// this will return all text inside html content even hidden one 
+// 'this is Document Object Modeltest text hello world!' outuput when console.log();
+
+
+// query selectors +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//querySelector()
+document.querySelector(".title")
+// It select first and only one in the HTML query 
+
+// querySelectorAll()
+document.querySelectorAll(".title")
+// it select all query with same id,classname and HTML tag and returns NodeList 
+
+// ++++++++++++++++++++++++++++++++++
+
+// querySelector with htmlTag 
+document.querySelector("h1")
+
+// querySelector with className 
+document.querySelector(".heading")
+
+// querySelector with id 
+document.querySelector("#heading")
+
+
+// nodeList vs HTMLCollection++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+
+//HTMLCollection
+// HTMLCollection returns collection of html and it doesn't provide any looping function 
+
+// nodeList 
+// nodeList return node list of html and it provides forEach looping function but it can't work as fully functional array
+
+
+// you can convert HTMLCollection and nodeList to an array 
+
+Array.from(document.querySelectorAll(".heading"))

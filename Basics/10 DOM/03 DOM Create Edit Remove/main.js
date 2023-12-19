7099646fd4370c48479564165434c27f4b599d4e
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2023-12-19 19:14:14 +0530

    commit

--- Basics/10 DOM/03 DOM Create Edit Remove/main.js
+++ Basics/10 DOM/03 DOM Create Edit Remove/main.js
@@ -7,18 +7,10 @@ console.log(newLi);
 // append to element's child 
 document.querySelector("ul").appendChild(newLi);
 
-// append to element using append 
-const a = document.createElement("a")
-a.innerText = "hello Sahil"
-const ulElmn = document.querySelector("ul")
-ulElmn.append(a)
-console.log(a);
-
 
 // edit Element
-// document.querySelector(".list").lastElementChild.innerHTML = "java";
+document.querySelector(".list").lastElementChild.innerHTML = "java";
 
 // remove element 
-
 const lastElm = document.querySelector(".list").firstElementChild.remove()
 console.log(lastElm);
---
+++ Basics/10 DOM/04 Events in Javascript/main.js
@@ -0,0 +1,9 @@
+// secendWay 
+const secWay = document.querySelector(".secWay");
+secWay.onclick = function() {return alert("hello world")};
+
+// third way
+const best = document.querySelector(".bestWay");
+best.addEventListener("click", function() {
+    return alert("hello Sahil")
+});

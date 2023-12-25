34ef351b33484e3b39ebf13544628919ec107b29
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2023-12-25 22:59:49 +0530

    commit

--- 02 Advanced/02 Promise/01 Promise.js
+++ 02 Advanced/02 Promise/01 Promise.js
@@ -1,18 +1,35 @@
 let Promise1 =  new Promise(function(resolve, reject) {
     setTimeout(function(){
-        console.log("task completed");
+        // console.log("task completed");
         resolve()
     }, 5000)
 })
 Promise1.then(function(){
-    console.log("work done");
+    // console.log("work done");
 });
 
 new Promise(function(resolve, reject) {
     setTimeout(() => {
-        console.log("task2 completed");
+        // console.log("task2 completed");
         resolve()
     }, 10000);
 }).then(function(){
-    console.log("work2 done");
+    // console.log("work2 done");
+});
+
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let error = true;
+        if(!error) {
+            console.log("fetch promise3 sucessful");
+            resolve()
+        }else{
+            console.log("error something went wrong");
+            reject()
+        }
+    }, 2000)
+}).then(() => {
+    console.log("work3 done");
+}).catch(()=>{
+    console.log("work3 failed");
 });

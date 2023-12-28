54ff315ec6431325202d5b081c72bb35b6b65592
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2023-12-28 21:01:15 +0530

    commit

---
+++ 01 Basics/11 Projects/03 New Year Calculator/main.js
@@ -0,0 +1,12 @@
+const newYear = "1 Jan 2024"
+
+function newyearcalculator(){
+    let milisec = new Date
+
+    console.log(milisec);
+}
+
+newyearcalculator()
+
+
+setInterval(newyearcalculator, 1000)
---
+++ 02 Advanced/05 Classes/02 Getters and Setters.js
@@ -0,0 +1,68 @@
+class users {
+    constructor(email, password) {
+        this.emailAddress = email;
+        this.userPassword = password;
+    }
+// The getter method is called when you try to access the property.
+// A getter is a special method within an object that allows you to retrieve the value of a property.
+// It is defined using the get keyword, followed by the property name, and a block of code that determines what happens when the property is accessed.
+    
+    get passwordUpperCase() {
+        return this.userPassword.toUpperCase()
+    }
+
+    
+    set (value) {
+        value = this.userPassword.toUpperCase()
+    }
+
+
+// The setter method is called when you try to assign a value to the property.  
+// A setter is a special method within an object that allows you to set the value of a property.
+// It is defined using the set keyword, followed by the property name, and a block of code that specifies what happens when the property is assigned a new value.  
+
+    // set password(value) {
+    //     return this._password = value.toUpperCase()
+    // }
+}
+
+const user = new users("shailmachhi29@gmail.com", "sahilsss")
+console.log(user);
+
+// example 2 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+    // get height(){
+    //     return this._height
+    // }
+    // get width(){
+    //     return this._width
+    // }
+
+    // set width(newWidth) {
+    //     if(newWidth > 0) {
+    //         newWidth = this._width;
+    //     }
+    //     else{
+    //         console.log("set width perfectly")
+    //     }
+    // }
+    // set height(newHeight) {
+    //     if(newHeight > 0) {
+    //         newHeight = this._height;
+    //     }
+    //     else{
+    //         console.log("set height perfectly")
+    //     }
+    // }
+
+}
+
+
+const shape = new rectangle(22, 212)
+// console.log(shape);

e35b2b0473de64935e70fc2670e1931c88f16422
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2023-12-29 14:29:39 +0530

    commit

--- 01 Basics/11 Projects/03 New Year Calculator/main.js
+++ 01 Basics/11 Projects/03 New Year Calculator/main.js
@@ -1,12 +1,39 @@
-const newYear = "1 Jan 2024"
+const newYearDate = new Date("Jan 1, 2024");
+let days = document.querySelector('.days');
+let Hours = document.querySelector('.hour');
+let Minutes = document.querySelector('.min');
+let secs = document.querySelector('.sec');
 
-function newyearcalculator(){
-    let milisec = new Date
+function newYearCalculator() {
+  let now = new Date();
+  let newYear = new Date(newYearDate);
 
-    console.log(milisec);
+  let countdown = Math.floor((newYear - now) / 1000);
+  let sec = countdown % 60;
+  let min = Math.floor((countdown / 60) % 60);
+  let hour = Math.floor((countdown / 3600) % 24);
+  let day = Math.floor(countdown / (3600 * 24));
+
+  days.innerText = addZero(day)
+  Hours.innerText = addZero(hour)
+  Minutes.innerText = addZero(min);
+  secs.innerText = addZero(sec);
+
+
+  function addZero(value){
+    console.log(value);
+    if(value < 10){
+      value = `0${value}`
+    }
+    return value
+  }
+  
+  if(day == 0 && hour == 0 && min == 0 && sec == 0){
+    
+  }
+
 }
 
-newyearcalculator()
+newYearCalculator();
 
-
-setInterval(newyearcalculator, 1000)
+let countdown = setInterval(newYearCalculator, 1000);

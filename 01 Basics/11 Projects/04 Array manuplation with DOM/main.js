b41abfd1fb6b9b40bf0ce87871adcbc63913b07f
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-02-15 22:00:24 +0530

    commit

--- 01 Basics/11 Projects/04 Array manuplation with DOM/main.js
+++ 01 Basics/11 Projects/04 Array manuplation with DOM/main.js
@@ -1,58 +1,60 @@
-let todoList = document.getElementById("todoList")
-let add = document.querySelector("#add")
-let remove = document.querySelector("#remove")
-let inputText = document.getElementById("inputText")
+let todoList = document.getElementById("todoList");
+let add = document.querySelector("#add");
+let remove = document.querySelector("#remove");
+let inputText = document.getElementById("inputText");
 
 
-let array = ["sahil", "rahil", "shailee"]
+let array = ["berlin", "jhon", "thomas"];
 console.log(array);
 
-
-let newArray
-let li
+let li;
 function renderArray() {
-
     if (todoList.hasChildNodes() === true) {
         todoList.innerHTML = '';
         array.forEach(items => {
-            li = document.createElement("li")
+            li = document.createElement("li");
             todoList.appendChild(li);
             li.innerText = items;
 
         });
     } else {
         array.forEach(items => {
-            li = document.createElement("li")
+            li = document.createElement("li");
             todoList.appendChild(li);
             li.innerText = items;
-
         });
     }
-}
-
+};
 renderArray()
 
 remove.addEventListener("click", () => {
     if (array.length === 0) {
-        alert("no name left")
+        alert("no name left");
     } else {
         todoList.removeChild(li);
         array.pop()
         console.log(
             array
         );
-        renderArray()
+        renderArray();
     }
-})
+});
 
 
 add.addEventListener("click", () => {
-
-
-
-})
-
-
-
-
-
+    let inputVal = inputText.value;
+    if (array.includes(inputVal)) {
+        alert("array has alredy this name");
+    }
+    else {
+        if (inputVal == "") {
+            alert("cannot add empty value");
+        }
+        else {
+            array.push(inputVal);
+            inputText.value = "";
+            renderArray();
+            console.log(array);
+        }
+    }
+});
